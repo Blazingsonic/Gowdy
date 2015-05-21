b1e59e60a1adbd31d57720f6fae826f4ff4926dc
sonic <sebastian.saier@web.de>
2015-05-21 12:41:42 +0200

    ImageRequest funktioniert.

--- app/src/main/java/com/example/sonic/gowdy/ui/MainActivity.java
+++ app/src/main/java/com/example/sonic/gowdy/ui/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.sonic.gowdy.ui;
 
 import android.app.Activity;
+import android.app.DownloadManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -9,12 +11,20 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 import com.example.sonic.gowdy.Kneipe;
 import com.example.sonic.gowdy.R;
 import com.example.sonic.gowdy.adapters.Kneipenadapter;
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import butterknife.ButterKnife;
@@ -25,17 +35,24 @@ public class MainActivity extends Activity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
 
+    private String mUrl = "http://webuser.hs-furtwangen.de/~taubew/android/image/schwarzwaldnebel1.jpg";
+    private String[] mTags = {"schwarzwaldnebel1.jpg", "schwarzwaldnebel2.jpg", "sonnenuntergang.jpg"};
+
     private RecyclerView.LayoutManager mLayoutManager;
 
     @InjectView(R.id.spinnerView) Spinner mSpinner;
     @InjectView(R.id.spinnerView2) Spinner mSpinner2;
     @InjectView(R.id.recyclerView) RecyclerView mRecyclerView;
+    @InjectView(R.id.imageRequest) ImageView mImageRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
+
+        // Get Image from request
+        Picasso.with(this).load(mUrl).into(mImageRequest);
 
         ArrayList<Kneipe> kneipen = getListData();
         mRecyclerView.setHasFixedSize(true); // Not always recommended, but in this case enhances performance
@@ -217,33 +234,6 @@ public class MainActivity extends Activity {
         kneipe.setName("Zum Bergbrau");
         kneipe.setAdresse("Kahnum 87, Stuttgart");
         kneipe.setTyp("Gute Weine");
-        kneipe.setBewertung("4");
-        kneipen.add(kneipe);
-
-        return kneipen;
-    }
-
-    private ArrayList<Kneipe> getListData2() {
-        ArrayList<Kneipe> kneipen = new ArrayList<Kneipe>();
-
-        Kneipe kneipe = new Kneipe();
-        kneipe.setName("Restaurant Giovanni");
-        kneipe.setAdresse("Birkenalle 2, Stuttgart");
-        kneipe.setTyp("Italienische Speisen");
-        kneipe.setBewertung("5");
-        kneipen.add(kneipe);
-
-        kneipe = new Kneipe();
-        kneipe.setName("Schnell und Zackig Imbiss");
-        kneipe.setAdresse("Schloss-Straße 54, Neukirch");
-        kneipe.setTyp("Türkisches Fast Food");
-        kneipe.setBewertung("3");
-        kneipen.add(kneipe);
-
-        kneipe = new Kneipe();
-        kneipe.setName("Taurin Bierstube");
-        kneipe.setAdresse("Henklegasse 13, Freiburg");
-        kneipe.setTyp("Bier brauen");
         kneipe.setBewertung("4");
         kneipen.add(kneipe);
 
